4e4c09bdd12bf69a40e10c8acac833d366d2b2eb
Sam <sam@thriveweb.com.au>
2018-11-16 09:51:50 +1000

    Add all CMS previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,13 +2,15 @@ import React from 'react'
 import CMS from 'netlify-cms'
 import './cms-utils'
 
-import { HomePageTemplate } from '../templates/HomePage'
 import { AboutPageTemplate } from '../templates/AboutPage'
-import { ContactPageTemplate } from '../templates/ContactPage'
-import { ProjectCategoryPageTemplate } from '../templates/ProjectCategoryPage'
-import { SingleProjectTemplate } from '../templates/SingleProject'
 import { BlogIndexTemplate } from '../templates/BlogIndex'
+import { ContactPageTemplate } from '../templates/ContactPage'
+import { HomePageTemplate } from '../templates/HomePage'
+import { ProjectCategoryPageTemplate } from '../templates/ProjectCategoryPage'
+import { ProjectPageTemplate } from '../templates/ProjectPage'
+import { SinglePhotographyTemplate } from '../templates/SinglePhotography'
 import { SinglePostTemplate } from '../templates/SinglePost'
+import { SingleProjectTemplate } from '../templates/SingleProject'
 
 if (
   window.location.hostname === 'localhost' &&
@@ -21,24 +23,30 @@ if (
   CMS.registerPreviewStyle('/styles.css')
 }
 
-CMS.registerPreviewTemplate('home-page', ({ entry }) => (
-  <HomePageTemplate {...entry.toJS().data} isPreview />
-))
 CMS.registerPreviewTemplate('about-page', ({ entry }) => (
   <AboutPageTemplate {...entry.toJS().data} isPreview />
-))
-CMS.registerPreviewTemplate('contact-page', ({ entry }) => (
-  <ContactPageTemplate {...entry.toJS().data} />
 ))
 CMS.registerPreviewTemplate('blog-page', ({ entry }) => (
   <BlogIndexTemplate {...entry.toJS().data} />
 ))
+CMS.registerPreviewTemplate('contact-page', ({ entry }) => (
+  <ContactPageTemplate {...entry.toJS().data} />
+))
+CMS.registerPreviewTemplate('home-page', ({ entry }) => (
+  <HomePageTemplate {...entry.toJS().data} isPreview />
+))
 CMS.registerPreviewTemplate('projectCategories', ({ entry }) => (
   <ProjectCategoryPageTemplate {...entry.toJS().data} />
 ))
-CMS.registerPreviewTemplate('projects', ({ entry }) => (
-  <SingleProjectTemplate {...entry.toJS().data} />
+CMS.registerPreviewTemplate('work-page', ({ entry }) => (
+  <ProjectPageTemplate {...entry.toJS().data} />
+))
+CMS.registerPreviewTemplate('photography', ({ entry }) => (
+  <SinglePhotographyTemplate {...entry.toJS().data} />
 ))
 CMS.registerPreviewTemplate('posts', ({ entry }) => (
   <SinglePostTemplate {...entry.toJS().data} />
+))
+CMS.registerPreviewTemplate('projects', ({ entry }) => (
+  <SingleProjectTemplate {...entry.toJS().data} />
 ))
