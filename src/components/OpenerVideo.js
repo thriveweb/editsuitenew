0287101000845875bb89078fbd2d6d37bdb17ebe
Sam <sam@thriveweb.com.au>
2018-12-14 10:43:40 +1000

    Preload

--- src/components/OpenerVideo.js
+++ src/components/OpenerVideo.js
@@ -9,6 +9,7 @@ const OpenerVideo = ({ src, title }) => (
       src={`https://player.vimeo.com/video/${src}?background=1&autoplay=1&loop=1&byline=0&title=0`}
       frameBorder="0"
       allow="autoplay; fullscreen"
+      rel="preload"
     />
   </div>
 )
