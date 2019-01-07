510a5d1633c808be092b86fc59bb62a1f01ed605
Vincent Weltje <vincentweltje@Vincents-iMac.local>
2019-01-08 09:43:15 +1000

    removed data attr from video

--- src/components/OpenerVideo.js
+++ src/components/OpenerVideo.js
@@ -4,7 +4,7 @@ import './Opener.css'
 
 const OpenerVideo = ({ src, title }) => (
   <div className="opener video">
-    <video data-keepplaying autoplay loop playsinline muted>
+    <video autoplay loop muted playsinline>
       <source src={src} type="video/mp4" />
     </video>
   </div>
