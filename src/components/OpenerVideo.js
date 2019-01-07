da62cd8c891008c67af0e7e9022e86924e8b1f85
Vincent Weltje <vincentweltje@Vincents-iMac.local>
2019-01-08 09:54:46 +1000

    test video

--- src/components/OpenerVideo.js
+++ src/components/OpenerVideo.js
@@ -4,10 +4,12 @@ import './Opener.css'
 
 const OpenerVideo = ({ src, title }) => (
   <div className="opener video">
-    <video autoplay loop muted playsinline>
-      <source src={src} type="video/mp4" />
+    <video autoPlay loop muted playsInline>
+      <source src="/images/test.mp4" type="video/mp4" />
     </video>
   </div>
 )
 
 export default OpenerVideo
+
+//<source src={src} type="video/mp4" />
